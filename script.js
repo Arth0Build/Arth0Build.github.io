2e617a8d0f7b33cd6722b16e0d556f694b843f9b
avp45 <arthvpatel77@gmail.com>
2024-03-04 18:19:01 -0500

    commit

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 
-/* Add this style to your existing styles */
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the image element
+
     var image = document.getElementById('profileImage');
-    
-    // Set the initial state
     image.style.opacity = 0;
     image.style.transform = 'translateX(-100%)';
-
-    // Trigger the animation after a short delay
     setTimeout(function () {
         image.classList.add('animated-image');
-    }, 500); // Adjust the delay as needed
+    }, 500);
 });
